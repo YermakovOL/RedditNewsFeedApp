3034d3162f5f88a0b4563244b2978d084f9605d1
Olexiy <6diamondsex@gmail.com>
2024-07-22 17:00:23 +0300

    Add TopAppBar with "retry" button

--- app/src/main/java/yermakov/oleksii/redditnewsfeed/RedditNewsFeedApp.kt
+++ app/src/main/java/yermakov/oleksii/redditnewsfeed/RedditNewsFeedApp.kt
@@ -1,11 +1,67 @@
 package yermakov.oleksii.redditnewsfeed
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import yermakov.oleksii.redditnewsfeed.ui.theme.RedditNewsFeedAppTheme
 
 @Composable
 fun RedditNewsFeedApp(
 modifier: Modifier = Modifier
 ) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+                TopBar()
+            },
+        ){innerPadding ->
+        Text(text = "Hello",
+            modifier = Modifier.padding(innerPadding))
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.secondary,
+        ),
+        title = {
+            Text("Reddit Top")
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = "Retry"
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RedditNewsFeedAppTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            RedditNewsFeedApp()
+        }
+    }
 }
